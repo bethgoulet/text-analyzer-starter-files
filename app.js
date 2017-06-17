372a6bacc2ad31759c5992f481432c60299312fa
bethgoulet <e.goulet@snhu.edu>
2017-06-17 17:45:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // your code here!
 
-function averageWordLength(words) {
-  var totalCharacters = words.join("").length;
-  return (totalCharacters / words.length).toFixed(1);
+function averageWordLength(tokens) {
+  var totalCharacters = tokens.join("").length;
+  return (totalCharacters / tokens.length).toFixed(3);
 }
 
-function uniqueWordCount(words) {
+function uniqueWordCount(tokens) {
   var uniqueCount = [];
-  for (var i=0; i < words.length; i++) {
-    if (uniqueCount.indexOf(words[i]) === -1) {
-      uniqueCount.push(words[i]);
+  for (var i=0; i < tokens.length; i++) {
+    if (uniqueCount.indexOf(tokens[i]) === -1) {
+      uniqueCount.push(tokens[i]);
     }
   }
   return uniqueCount.length;
@@ -23,25 +23,27 @@ function removeReturns(text) {
   return text.replace(/\r?\n|\r/g, "");
 }
 
-function displayResults(text){
-    var words = getWords(text);
-    var wordCount = words.length;
-    var uniqueCount = uniqueWordCount(words);
-    var avgLength = averageWordLength(words);
+function displayResults(text) {
+    var tokens = getWords(text);
+    var wordCount = tokens.length;
+    var uniqueCount = uniqueWordCount(tokens);
+    var avgLength = averageWordLength(tokens);
   
-    $('.js-report').find('.js-word-count').text(wordCount);
-    $('.js-report').find('.js-unique-word-count').text(uniqueCount);
-    $('.js-report').find('.js-average-word-length').text(avgLength);
+    var finalReport = $('.js-report');
+  
+    finalReport.find('.js-word-count').text(wordCount);
+    finalReport.find('.js-unique-word-count').text(uniqueCount);
+    finalReport.find('.js-average-word-length').text(avgLength);
     
-    $('.js-report').removeClass('hidden');
+    finalReport.removeClass('hidden');
 }
 
 function analyzeText() {
   $('.js-form').submit(function(event) {
     event.preventDefault();
-  var text = $(this).find('#user-text').val();
-  displayResults(removeReturns(text));
-});
+    var inputText = $(this).find('#user-text').val();
+    displayResults(removeReturns(inputText));
+  });
 }
                        
 $(function() {
