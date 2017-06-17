9e78032d4339d60df873c59f67888aad404243c6
bethgoulet <e.goulet@snhu.edu>
2017-06-17 17:29:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 // your code here!
 
-function getWords(text) {
-  return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
-}
-
-function removeReturns(text) {
-  return text.replace(/\r?\n|\r/g, "");
-}
-
 function averageWordLength(words) {
   var totalCharacters = words.join("").length;
   return (totalCharacters / words.length).toFixed(1);
@@ -23,16 +15,25 @@ function uniqueWordCount(words) {
   return uniqueCount.length;
 }
 
+function getWords(text) {
+  return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
+}
+
+function removeReturns(text) {
+  return text.replace(/\r?\n|\r/g, "");
+}
+
 function displayResults(text){
-    var wordCount = getWords(text).length;
-    var uniqueCount = uniqueWordCount(text);
-    var avgLength = averageWordLength(text);
+    var words = getWords(text);
+    var wordCount = words.length;
+    var uniqueCount = uniqueWordCount(words);
+    var avgLength = averageWordLength(words);
   
     $('.js-report').find('.js-word-count').text(wordCount);
     $('.js-report').find('.js-unique-word-count').text(uniqueCount);
     $('.js-report').find('.js-average-word-length').text(avgLength);
     
-  $('.js-report').removeClass('hidden');
+    $('.js-report').removeClass('hidden');
 }
 
 function analyzeText() {
